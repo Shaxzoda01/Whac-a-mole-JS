bdca56be688294a20b33544bf5433ca8fd864b29
shaxzoda01 <shaxzoda01062005@gmail.com>
2025-10-05 15:59:56 +0500

    fixed some issues

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ var input = document.getElementById('input')
 var resultTable = document.getElementById('results')
 var hammer = document.getElementById("hammer");
 var highScoreContainer = document.getElementById("highScore")
+console.log(circles)
 
 var score = 0;
+var gameActive = false;
 
 function startGame() {
     hammer.style.display = "block";
     document.body.style.cursor = "none";
 
-    // userName = input.value;
+    userName = input.value;
     score = 0;
     updateScore();
+    gameActive = true;
     nextCircle();
 
     document.addEventListener("mousemove", (e) => {
@@ -28,32 +31,52 @@ function startGame() {
     });
 
     setTimeout(() => {
-        circles.forEach(circle => {
+        circles.forEach(function (circle) {
             circle.classList.remove('active');
             circle.onclick = null;
         });
-        resultTable.insertAdjacentHTML('beforeend', `<tr><td>${score}</td></tr>`); //<td>${userName}</td>
+
+        resultTable.insertAdjacentHTML('beforeend', `<td>${userName}</td> <tr><td>${score}</td></tr>`);
+
+
+        document.body.style.cursor = "default";
+        hammer.style.display = "none";
+        score = 0;
+        gameActive = false;
+        updateScore();
     }, 20000);
 
 }
 
 function nextCircle() {
-    circles.forEach(circle => {
+    if (gameActive === false) {
+        return;
+    }
+    circles.forEach(function (circle) {
         circle.classList.remove('active');
         circle.onclick = null;
     });
+    var molesToShow = Math.floor(Math.random() * 2) + 1;
+    for (let i = 0; i < molesToShow; i++) {
+        let randomIndex = Math.floor(Math.random() * circles.length);
+        let activeCircle = circles[randomIndex];
 
-    var randomIndex = Math.floor(Math.random() * circles.length);
-    var activeCircle = circles[randomIndex];
-    activeCircle.classList.add('active');
+        activeCircle.classList.add('active');
 
-    activeCircle.onclick = () => {
-        activeCircle.onclick = null;
-        activeCircle.classList.remove('active');
-        score++;
-        updateScore();
-        setTimeout(nextCircle, 300);
-    };
+        // При клике добавляем очки
+        activeCircle.onclick = () => {
+            activeCircle.onclick = null;
+            activeCircle.classList.remove('active');
+            score++;
+            updateScore();
+        };
+
+        // Через 700 мс крот сам исчезнет
+        setTimeout(() => {
+            activeCircle.classList.remove('active');
+        }, 1000);
+    }
+    setTimeout(nextCircle, 1000);
 }
 function updateScore() {
     scoreDisplay.textContent = score;
